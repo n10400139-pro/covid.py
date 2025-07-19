aed7c606fe18dd1dc188e361dbae8a9a76df221d
n10400139-pro <n10400139@gmail.com>
2025-07-19 10:53:55 +0530

    Add files via upload

---
+++ covid.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import requests
+
+st.set_page_config(
+    page_title = "🦠 COVID-19 Tracker",
+    page_icon = "🏠",
+    layout = "wide"
+)
+
+st.sidebar.title("My Covid Tracker")
+st.title("🦠 COVID-19 Tracker")
+
+country = st.text_input("Enter country name:", "Pakistan")
+
+if st.button("Get Data"):
+    url = f"https://disease.sh/v3/covid-19/countries/{country}"
+    response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    st.metric("Total Cases", data["cases"])
+    st.metric("Deaths", data["deaths"])
+    st.metric("Recovered", data ["recovered"])
+else:
+    st.error("Country not found")
+
